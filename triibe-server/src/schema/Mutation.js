d74b4d1cddb995a08600216ee3a104256239507c
Jack Bennett <jack@soap.productions>
2019-10-13 18:37:15 +1100

    Chat Creation and Message Posting

--- triibe-server/src/schema/Mutation.js
+++ triibe-server/src/schema/Mutation.js
@@ -1,10 +1,52 @@
-const { objectType } = require('nexus')
+const { objectType, idArg, stringArg } = require('nexus')
 
 const Mutation = objectType({
   name: 'Mutation',
   definition(t) {
-    t.crud.createOneUser({ alias: 'signUpUser' })
-    t.crud.deleteOneUser()
+    t.crud.createOneUser()
+    t.crud.updateOneUser()
+
+    t.field('createChat', {
+      type: "Chat",
+      args: {
+        users: idArg({list: true})
+      },
+      resolve: (root, { users }, ctx) => {
+        return ctx.photon.chats.create({ 
+          data: {
+            users: {
+              connect: users.map( user => { return {id: user} })
+            }
+          },
+          include: {
+            users: true
+          } 
+        })
+      }
+    })
+    t.field('postMessage', {
+      type: "Message",
+      args: {
+        sentBy: idArg(),
+        sentTo: idArg(),
+        content: stringArg()
+      },
+      resolve: (root, { sentBy, sentTo, content }, ctx) => {
+        return ctx.photon.chats.update({
+          where: { id: sentTo },
+          data: {
+            messages: {
+              create: {
+                sentBy: {
+                  connect: {id: sentBy}
+                },
+                content
+              }
+            }
+          }
+        })
+      }
+    })
   }
 })
 
--- triibe-server/src/schema/Query.js
+++ triibe-server/src/schema/Query.js
@@ -5,6 +5,8 @@ const Query = objectType({
   definition(t) {
     t.crud.user()
     t.crud.users()
+    t.crud.chat()
+    t.crud.chats()
   }
 })
 
